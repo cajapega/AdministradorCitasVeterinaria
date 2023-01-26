9976c19e1cf19fa28818fe7d4191ec2be0253f74
Carlos Perez <charlyjun@gmail.com>
2023-01-25 21:43:30 -0500

    creo listeners

--- js/script.js
+++ js/script.js
@@ -12,11 +12,27 @@ const contenedorCitas = document.querySelector('#citas');
 //Para cada Input voy a tener un liseners
 eventListeners();
 function eventListeners() { //llamo la funcion
-    mascotaInput.addEventListener('change', datosCita)
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita)
+    sintomasInput.addEventListener('input', datosCita)
+}
+
+
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintoma: ''
 }
 
 function datosCita(e) {
-    console.log(e.target.value) //con esto veo lo que se escribe
+    citaObj[e.tarjet.name] = e.tarjet.value; //de esta manera accedo a las propiedades
+    console.log(citaObj);
 }
 
 //la funcion llamada datosCita se va a ir llamando cada vez
