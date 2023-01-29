a651ff6c4ec217614c5ed6149c6e8d9d05123509
Carlos Perez <charlyjun@gmail.com>
2023-01-28 19:48:24 -0500

    Listeners funcionales, se guarda en el objeto por medio del atributo name el valor de entrada y agrego un favicon

--- js/script.js
+++ js/script.js
@@ -5,21 +5,20 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
-const formulario = document.querySelector('#ueva-cita');
+//const formulario = document.querySelector('#ueva-cita');
 
-const contenedorCitas = document.querySelector('#citas');
+//const contenedorCitas = document.querySelector('#citas');
 
-//Para cada Input voy a tener un liseners
+//Para cada Input voy a tener un liseners, la mano a llamar
 eventListeners();
 function eventListeners() { //llamo la funcion
-    mascotaInput.addEventListener('input', datosCita);
+    mascotaInput.addEventListener("input", datosCita);
     propietarioInput.addEventListener('input', datosCita);
     telefonoInput.addEventListener('input', datosCita);
     fechaInput.addEventListener('input', datosCita);
-    horaInput.addEventListener('input', datosCita)
-    sintomasInput.addEventListener('input', datosCita)
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
 }
-
 
 const citaObj = {
     mascota: '',
@@ -27,13 +26,16 @@ const citaObj = {
     telefono: '',
     fecha: '',
     hora: '',
-    sintoma: ''
+    sintomas: ''
 }
 
 function datosCita(e) {
-    citaObj[e.tarjet.name] = e.tarjet.value; //de esta manera accedo a las propiedades
+    citaObj[e.target.name] = e.target.value;
     console.log(citaObj);
+    //console.log(e.target.name)
+    /*e es el objeto que representa el evento, y uno de sus atributos es el target, 
+    que viene a ser el elemento que recibió el evento. (por ej un botón). Podemos especificar 
+    que se acceda por atributo "e.target.name" en este caso el atributo name ej mascota 
+    pero si pongo e.tarjet.value accedera por el valor que se esta escribiendo en la caja de 
+    texto. Tambien puedo poner e.target.id y accedere por el atributo id del elemento html*/  
 }
-
-//la funcion llamada datosCita se va a ir llamando cada vez
-//que yo vaya escribiendo algo
